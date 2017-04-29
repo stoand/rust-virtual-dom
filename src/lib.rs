13f4ce0b3cb0d1ca2125a081b7b52e2786ac7a46
Andreas Stocker <shadowstep7@gmail.com>
2017-04-29 15:07:33 +0200

    fix template binding

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,31 @@
 use std::collections::HashMap;
 
-#[derive(Debug,PartialEq,Eq)]
-pub struct VirtualDom<'a>(Vec<VirtualNode<'a>>);
+#[derive(Debug,PartialEq,Eq,Clone)]
+pub struct VirtualDom(Vec<VirtualNode>);
 
-impl<'a, T: ToString> From<T> for VirtualDom<'a> {
-    fn from(s: T) -> VirtualDom<'a> {
+impl<'a, T: ToString> From<T> for VirtualDom {
+    fn from(s: T) -> VirtualDom {
         VirtualDom(vec![VirtualNode::Text(s.to_string())])
     }
 }
 
 #[derive(Debug,PartialEq,Eq,Clone)]
-pub enum VirtualNode<'a> {
+pub enum VirtualNode {
     Text(String),
-    Element(VirtualElement<'a>),
+    Element(VirtualElement),
 }
 
 #[derive(Debug,PartialEq,Eq,Clone)]
-pub struct VirtualElement<'a> {
-    name: &'a str,
-    attributes: HashMap<&'a str, &'a str>,
-    child_nodes: Vec<VirtualNode<'a>>,
+pub struct VirtualElement {
+    name: String,
+    attributes: HashMap<String, String>,
+    child_nodes: Vec<VirtualNode>,
 }
 
-impl<'a> VirtualElement<'a> {
+impl VirtualElement {
     fn new() -> Self {
         VirtualElement {
-            name: "",
+            name: "div".to_string(),
             attributes: HashMap::new(),
             child_nodes: Vec::new(),
         }
@@ -42,34 +42,31 @@ macro_rules! template {
 }
 
 macro_rules! inner_template {
-    ($tl:ident, ) => (|_: &mut::VirtualElement| vec![]);
+    ($tl:ident, ) => (|_: &mut::VirtualElement| Vec::<::VirtualNode>::new());
     (not_top_level, [$($key:ident=$val:expr)*]$($inner:tt)*) => (|el: &mut::VirtualElement| {
-        $(el.attributes.insert(stringify!($key), $val);)*
+        $(el.attributes.insert(stringify!($key).to_string(), $val.to_string());)*
         inner_template!(not_top_level, $($inner)*)(&mut el)
     });
     ($tl:ident, >($($inner_parens:tt)*)$($inner:tt)*) => (|el: &mut::VirtualElement| {
         let mut el_parens = ::VirtualElement::new();
-        let el_parens_additional = inner_template!(not_top_level, $($inner)*)(&mut el_parens);
+        let mut el_parens_additional = inner_template!(not_top_level, $($inner)*)(&mut el_parens);
         el.child_nodes.push(::VirtualNode::Element(el_parens));
-        for additional in el_parens_additional {
-            el.child_nodes.push(::VirtualNode::Element(additional));
-        }
+        el.child_nodes.append(&mut el_parens_additional);
 
         let mut el_remaining = ::VirtualElement::new();
-        let el_remaining_additional = inner_template!(not_top_level, $($inner)*)(&mut el_remaining);
+        let mut el_remaining_additional = inner_template!(not_top_level, $($inner)*)(&mut el_remaining);
         el.child_nodes.push(::VirtualNode::Element(el_remaining));
-        for additional in el_remaining_additional {
-            el.child_nodes.push(::VirtualNode::Element(additional));
-        }
+        el.child_nodes.append(&mut el_remaining_additional);
 
         Vec::<::VirtualNode>::new()
     });
     ($tl:ident, >$($inner:tt)*) => (|el: &mut::VirtualElement| {
         let mut el_remaining = ::VirtualElement::new();
-        let el_remaining_additional = inner_template!(not_top_level, $($inner)*)(&mut el_remaining);
+        let mut el_remaining_additional = inner_template!(not_top_level, $($inner)*)(&mut el_remaining);
         el.child_nodes.push(::VirtualNode::Element(el_remaining));
+        el.child_nodes.append(&mut el_remaining_additional);
 
-        vec![el_remaining_additional]
+        Vec::<::VirtualNode>::new()
     });
     (not_top_level, +($($inner_parens:tt)*)$($inner:tt)*) => (|_: &mut::VirtualElement| {
         let mut el_parens = ::VirtualElement::new();
@@ -81,10 +78,10 @@ macro_rules! inner_template {
 
         let mut els = Vec::new();
 
-        els.push(el_parens);
+        els.push(::VirtualNode::Element(el_parens));
         els.append(&mut el_parens_additional);
 
-        els.push(el_remaining);
+        els.push(::VirtualNode::Element(el_remaining));
         els.append(&mut el_remaining_additional);
         els
     });
@@ -95,41 +92,62 @@ macro_rules! inner_template {
 
         let mut els = Vec::new();
 
-        els.push(el_remaining);
+        els.push(::VirtualNode::Element(el_remaining));
         els.append(&mut el_remaining_additional);
         els
     });
+    ($tl:ident, {$bind:expr}$($inner:tt)*) => (|el: &mut::VirtualElement| {
+        el.child_nodes.append(&mut ::VirtualDom::from($bind).0);
+        inner_template!($tl, $($inner)*)(el)
+    });
     ($tl:ident, .$classes:ident$($inner:tt)*) => (|el: &mut::VirtualElement| {
         let classes = if let Some(existing_classes) = el.attributes.get("class") {
-            &(existing_classes.to_string() + " " + stringify!($classes))
+            existing_classes.to_string() + " " + stringify!($classes)
         } else {
-            stringify!($classes)
+            stringify!($classes).to_string()
         };
-        el.attributes.insert("class", classes);
+        el.attributes.insert("class".to_string(), classes);
         inner_template!($tl, $($inner)*)(el)
     });
     ($tl:ident, #$id:ident$($inner:tt)*) => (|el: &mut::VirtualElement| {
-        el.attributes.insert("id", stringify!($id));
+        el.attributes.insert("id".to_string(), stringify!($id).to_string());
         inner_template!($tl, $($inner)*)(el)
     });
     ($tl:ident, $name:ident$($inner:tt)*) => (|el: &mut::VirtualElement| {
-        el.name = stringify!($name);
+        el.name = stringify!($name).to_string();
         inner_template!($tl, $($inner)*)(el)
-    });
-    ($tl:ident, $bind:block$($inner:tt)*) => (|el: &mut::VirtualElement| {
-        el.child_nodes.nodes().append(&mut ::VirtualDom::from($bind).nodes());
-        inner_template!($tl, $($inner)*)(&mut el)
     });
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn template_macro() {
-        let a = template!(#asdf);
-        let t = template!(.video>.sidebar>(.asdf+.a[href="asdf" type="asdf"]@"inner text")+.a+(.b));
-        let t1 =
-            template!(.video>.sidebar>(.asdf#a+.a[href="asdf" type="asdf"]@"inner text")+.a+(.b));
-        assert_eq!(t, t1);
+    fn template_name_class_id() {
+        let mut el = VirtualElement::new();
+        assert_eq!(el, template!(div));
+
+        el.name = "a".to_string();
+        assert_eq!(el, template!(a));
+
+        el.attributes.insert("class".into(), "active red".into());
+        assert_eq!(el, template!(a.active.red));
+
+        el.attributes.insert("id".into(), "main".into());
+        assert_eq!(el, template!(a#main.active.red));
     }
+
+    #[test]
+    fn template_binding() {
+        let mut el = VirtualElement::new();
+        el.child_nodes.push(VirtualNode::Text("some inner text".into()));
+        assert_eq!(el, template!(div{"some inner text"}));
+    }
+
+
+        // let t = template!(.video>.sidebar>(.asdf+.a[href="asdf" type="asdf"]@"inner text")+.a+(.b));
+        // let t1 =
+        //     template!(.video>.sidebar>(.asdf#a+.a[href="asdf" type="asdf"]@"inner text")+.a+(.b));
+        // assert_eq!(t, t1);
 }
